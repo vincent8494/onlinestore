fdc6ed82793f786334be5a35f133636ed07a8109
vincent8494 <fettymikev@gmail.com>
2025-06-13 22:13:11 +0300

    Update configuration for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,29 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+          vendor: ["@radix-ui/*"],
+        },
+      },
+    },
+  },
+  server: {
+    host: "::",
+    port: 8080,
   },
 });
